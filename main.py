b306dde8098a316a91dad71e31032e184deb1874
Yatik1 <yatiksrivastava1@gmail.com>
2025-04-22 17:50:26 +0530

    simpler version

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from openai import OpenAI
+import os
+from pydantic import BaseModel
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials=True, 
+    allow_methods = ["*"],
+    allow_headers=["*"]
+)
+
+client = OpenAI(
+    api_key=os.getenv("API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+class Prompt(BaseModel):
+    message:str
+
+@app.post("/chats")
+def chat(prompt:Prompt):
+    response = client.chat.completions.create(
+        model="gemini-2.0-flash",
+        messages=[{"role":"user", "content": prompt.message}]
+    )
+
+    return {"response": response.choices[0].message.content}
