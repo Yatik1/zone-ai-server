d8a26ce1018a0584a21f714cd4390cc8c5c7bce6
Yatik1 <yatiksrivastava1@gmail.com>
2025-06-29 12:55:52 +0530

    Refactor config.py to load MODEL_NAME and AWS credentials from environment variables

--- config.py
+++ config.py
@@ -7,4 +7,9 @@ GEMINI_SECRET_KEY = os.getenv("GEMINI_SECRET_KEY")
 
 QDRANT_URL = "http://localhost:6333"
 
-MODEL_NAME="gemini-2.0-flash"
+MODEL_NAME=os.getenv("MODEL_NAME")
+
+BUCKET_NAME = os.getenv("BUCKET_NAME")
+REGION_NAME = os.getenv("REGION_NAME")
+ACCESS_KEY = os.getenv("ACCESS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
